465b96b4d44e2fb21d8da886803aa3dce53d849f
Emil79 <emilsamo@gmail.com>
2020-12-12 23:25:28 +0100

    Blog single done and Contact started

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    //ANIMATION
+    function animation() {
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+
+
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
+
+    if ($('.profession-slider').length > 0) {
+        $('.profession-slider').owlCarousel({
+
+            loop: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+
+                992: {
+                    items: 1,
+                    margin: 30
+                }
+
+            }
+        });
+
+    }
     
-    
-    
-    
-})
+        if ($('.blog-slider').length > 0) {
+        $('.blog-slider').owlCarousel({
+
+            loop: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+
+                992: {
+                    items: 1,
+                    margin: 30
+                }
+
+            }
+        });
+
+    }
+
+
+
+});
 
